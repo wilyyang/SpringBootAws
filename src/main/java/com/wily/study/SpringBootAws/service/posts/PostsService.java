47055f82ffe1e4b83251a346f63cf22c02a9afaa
wily <ehdrnr1178@gmail.com>
2021-12-10 18:25:14 +0900

    page 176

--- src/main/java/com/wily/study/SpringBootAws/service/posts/PostsService.java
+++ src/main/java/com/wily/study/SpringBootAws/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.wily.study.SpringBootAws.service.posts;
 
 import com.wily.study.SpringBootAws.domain.posts.Posts;
 import com.wily.study.SpringBootAws.domain.posts.PostsRepository;
+import com.wily.study.SpringBootAws.web.dto.PostsListResponseDto;
 import com.wily.study.SpringBootAws.web.dto.PostsResponseDto;
 import com.wily.study.SpringBootAws.web.dto.PostsSaveRequestDto;
 import com.wily.study.SpringBootAws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +33,16 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
     }
 }
